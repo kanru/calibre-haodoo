cff793c43c56a868207e499915600d21fccdbaf5
Kan-Ru Chen <kanru@kanru.info>
2010-09-26 08:24:59 +0800

    Update email address

--- haodoo/plugin.py
+++ haodoo/plugin.py
@@ -164,7 +164,7 @@ class HaoDooPdb(InputFormatPlugin):
     name                = 'HaoDoo PDB Plugin'
     description         = 'Add HaoDoo PDB/uPDB support to core PDB plugin'
     supported_platforms = ['windows', 'osx', 'linux']
-    author              = 'Kan-Ru Chen <kanru@0xlab.org>'
+    author              = 'Kan-Ru Chen <kanru@kanru.info>'
     file_types          = set(['updb'])
     version             = (0, 3, 4)
 
